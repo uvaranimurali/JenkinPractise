69444076d14ad470dd93f14c2abcb7303dc4a1c8
uvaranimurali <154619920+uvaranimurali@users.noreply.github.com>
2023-12-22 22:17:49 +0530

    Update jenkins.java

--- jenkins.java
+++ jenkins.java
@@ -1,5 +1,8 @@
 class jenkins {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+        System.out.println("Hello, World1!"); 
+        System.out.println("Hello, World2!"); 
+        System.out.println("Hello, World3!"); 
     }
 }
